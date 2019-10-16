4381019a23c0722e85a6ccdc8d5b5dd91f36fb43
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 18:17:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   def self.new_by_filename(filename)
     song_parts = filename.split(" - ")
     new_song = Song.new(song_parts[0])
-    new_song.artist_name(name)
+    new_song.artist_name(song_parts[1])
   end
   
   def artist_name(name)
